2e3d15be961002c69696458928ac7a4c0011beb7
= <1048217874@qq.com>
2017-11-15 19:20:58 +0800

    Finish Login and Registration, currently not support session tracking

--- src/main/java/com/peng1m/springboot/controller/LoginController.java
+++ src/main/java/com/peng1m/springboot/controller/LoginController.java
@@ -1,67 +1,22 @@
 package com.peng1m.springboot.controller;
 
-import com.peng1m.springboot.model.User;
-import com.peng1m.springboot.service.impl.UserServiceImpl;
-import com.peng1m.springboot.util.CustomErrorType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
-
-@RequestMapping(value = "/api/user")
-@RestController
-public class LoginController {
-    public static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-
-    @Autowired
-    UserServiceImpl userServiceImpl;
-
-    @RequestMapping(value = "/login/", method =  RequestMethod.POST)
-    public ResponseEntity<?> Login(@RequestBody User user, HttpServletResponse response){
-        logger.info("User {} is trying to login", user);
-        if (userServiceImpl.verifyUser(user.getName(), user.getPassword())){
-            String id = userServiceImpl.findByName(user.getName()).getId() + "";
-            Cookie cookie = new Cookie("userId", id);
-            cookie.setHttpOnly(true);
-            cookie.setMaxAge(30*60); // expired after 30 minutes
-            response.addCookie(cookie);
-            return new ResponseEntity<String>(id, HttpStatus.OK);
-        }
-        else {
-            logger.info("User name or password are incorrect! User: {}", user.getName());
-            return new ResponseEntity(new
-                    CustomErrorType("User name or password are incorrect!"), HttpStatus.NOT_FOUND);
-        }
-
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
     }
 
-    @RequestMapping(value = "/registered/", method = RequestMethod.POST)
-    public ResponseEntity<?> Registered(@RequestBody User user, HttpServletResponse response, UriComponentsBuilder ucBuilder){
-        logger.info("Registered User: {}", user);
+    @GetMapping("registered")
+    public String registered(){
+        return "registered";
+    }
 
-        if (userServiceImpl.findByName(user.getName()) != null) {
-            logger.error("Unable to create. A User with name {} already exist", user.getName());
-            return new ResponseEntity(new CustomErrorType("Unable to registered. A User with name " +
-                    user.getName() + " already exist."),HttpStatus.CONFLICT);
-        }
-        userServiceImpl.addUser(user);
-
-        String id = userServiceImpl.findByName(user.getName()).getId() + "";
-        Cookie cookie = new Cookie("userId", id);
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(30*60); // expired after 30 minutes
-        response.addCookie(cookie);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(ucBuilder.path("/api/user/{id}").buildAndExpand(user.getId()).toUri());
-        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+    @GetMapping("login")
+    public String login(){
+        return "login";
     }
 }
