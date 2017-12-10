3c9713dd5716921bad9b83d768b2dabd39fbc3eb
WangGY <11510050@mail.sustc.edu.cn>
2017-12-10 20:40:44 +0800

    Add a JSON tool packet. Some work on school api

---
+++ src/main/java/com/peng1m/springboot/repository/SchoolRepository.java
@@ -0,0 +1,12 @@
+package com.peng1m.springboot.repository;
+
+import com.peng1m.springboot.model.School;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface SchoolRepository extends CrudRepository<School, Integer> {
+    public School findById(int id);
+
+}
