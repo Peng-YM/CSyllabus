ebc53f6a1c42464d73b735159cc1ee2fdc5b8dd5
Peng-YM <1048217874pengym@gmail.com>
2017-12-17 22:11:29 +0800

    Initialized Project Skeleton

--- Front-End-App/src/app/app.module.ts
+++ Front-End-App/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SchoolDashboardComponent } from './school-dashboard/school-dashboard.component';
+import { SchoolDetailComponent } from './school-detail/school-detail.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { SchoolService } from './school.service';
+import { UserService } from './user.service';
+import { CourseService } from './course.service';
+import { SchoolRoutingModule } from './school-routing/school-routing.module';
+import { CourseRoutingModule } from './course-routing/course-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolDashboardComponent,
+    SchoolDetailComponent,
+    CourseDetailComponent,
+    UserDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SchoolRoutingModule,
+    CourseRoutingModule
   ],
-  providers: [],
+  providers: [SchoolService, UserService, CourseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Front-End-App/src/app/school-dashboard/school-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-dashboard',
+  templateUrl: './school-dashboard.component.html',
+  styleUrls: ['./school-dashboard.component.css']
+})
+export class SchoolDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
